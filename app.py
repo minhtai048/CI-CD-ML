ef8de3fca9a4afbaa170b8e8a46d214c37e4b321
minhtai048 <minhtai04082003@gmail.com>
2024-04-20 07:21:18 +0000

    Update with new results

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#APPLICATION FOR MEDICAL COST PREDICTION IN FOUR BIGGEST REGIONS IN USA. 
+# APPLICATION FOR MEDICAL COST PREDICTION IN FOUR BIGGEST REGIONS IN USA.
 
 """
 !!!DISCLAIMER!!
@@ -12,49 +12,68 @@ from flask import Flask, request, render_template
 from utils.preprocess import inverse_transform, replace_categories
 import pickle
 
-#Create an app object using the Flask class. 
+# Create an app object using the Flask class.
 app = Flask(__name__)
-#Load the trained model and encoder. (Pickle file)
-model = pickle.load(open('models/svr_model.pkl', 'rb'))
-scaler = pickle.load(open('models/mm_encoder.pkl', 'rb'))
-lmbda_charges = pickle.load(open('models/lmbda_price.pkl', 'rb'))
+# Load the trained model and encoder. (Pickle file)
+model = pickle.load(open("models/svr_model.pkl", "rb"))
+scaler = pickle.load(open("models/mm_encoder.pkl", "rb"))
+lmbda_charges = pickle.load(open("models/lmbda_price.pkl", "rb"))
 
-#Define the route to be home. 
-#Here, home function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-#use the route() decorator to tell Flask what URL should trigger our function.
-@app.route('/')
+
+# Define the route to be home.
+# Here, home function is with '/', our root directory.
+# Running the app sends us to index.html.
+# Note that render_template means it looks for the file in the templates folder.
+# use the route() decorator to tell Flask what URL should trigger our function.
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-#GET: A GET message is send, and the server returns data
-#POST: Used to send HTML form data to the server.
-#Add Post method to the decorator to allow for form submission. 
-#Redirect to /predict page with the output
-@app.route('/predict',methods=['POST']) #Do not add two method may make app crashed
+
+# GET: A GET message is send, and the server returns data
+# POST: Used to send HTML form data to the server.
+# Add Post method to the decorator to allow for form submission.
+# Redirect to /predict page with the output
+@app.route("/predict", methods=["POST"])  # Do not add two method may make app crashed
 def predict():
-    #input section
-    ageinput = request.form.get('ageinput')
-    genderinput = request.form.get('genderinput')
-    bmiinput = request.form.get('bmiinput')
-    childinput = request.form.get('childinput')
-    smokinginput = request.form.get('smokinginput')
-    regioninput = request.form.get('regioninput')   
+    # input section
+    ageinput = request.form.get("ageinput")
+    genderinput = request.form.get("genderinput")
+    bmiinput = request.form.get("bmiinput")
+    childinput = request.form.get("childinput")
+    smokinginput = request.form.get("smokinginput")
+    regioninput = request.form.get("regioninput")
     gender_display = genderinput
 
-    #feature transformation section
-    features = pd.DataFrame({"age":[ageinput], "sex":[genderinput], "bmi":[bmiinput], 
-                             "children":[childinput], "smoker":[smokinginput], "region":[regioninput]})    
+    # feature transformation section
+    features = pd.DataFrame(
+        {
+            "age": [ageinput],
+            "sex": [genderinput],
+            "bmi": [bmiinput],
+            "children": [childinput],
+            "smoker": [smokinginput],
+            "region": [regioninput],
+        }
+    )
     features = replace_categories(features)
-    features = scaler.transform(features)   
+    features = scaler.transform(features)
     prediction = model.predict(features)
     prediction = inverse_transform(prediction, lmbda_charges)
     prediction = np.round_(prediction, 2)[0]
-    
-    #final section -> send data back to front page
-    return render_template('index.html', age=ageinput, gender=gender_display, bmi=bmiinput, child=childinput, 
-                           smoking=smokinginput, region=regioninput, prediction_text= prediction)
+
+    # final section -> send data back to front page
+    return render_template(
+        "index.html",
+        age=ageinput,
+        gender=gender_display,
+        bmi=bmiinput,
+        child=childinput,
+        smoking=smokinginput,
+        region=regioninput,
+        prediction_text=prediction,
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- train.py
+++ train.py
@@ -16,44 +16,53 @@ from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import r2_score
 
-scaler = pickle.load(open('models/mm_encoder.pkl', 'rb'))
-lmbda_charges = pickle.load(open('models/lmbda_price.pkl', 'rb'))
+scaler = pickle.load(open("models/mm_encoder.pkl", "rb"))
+lmbda_charges = pickle.load(open("models/lmbda_price.pkl", "rb"))
+
 
 def load_and_process():
     data = pd.read_csv("data/metadata.csv")
     data = replace_categories(data)
-    data['charges_fixed'], lmbda_price = stats.yeojohnson(data['charges'])
-    X = data.drop(columns=['charges', 'charges_fixed'])
-    y = data['charges_fixed']
+    data["charges_fixed"], lmbda_price = stats.yeojohnson(data["charges"])
+    X = data.drop(columns=["charges", "charges_fixed"])
+    y = data["charges_fixed"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
     return X_train, X_test, y_train, y_test
+
 
 def train_and_evaluate():
     # prepare data for training
     X_train, X_test, y_train, y_test = load_and_process()
 
     # define model and stuffs for evaluation
-    models= [LinearRegression(), DecisionTreeRegressor(), 
-             RandomForestRegressor(), KNeighborsRegressor(n_neighbors=11), 
-             AdaBoostRegressor(), XGBRegressor(), SVR()]
-    scores= []
+    models = [
+        LinearRegression(),
+        DecisionTreeRegressor(),
+        RandomForestRegressor(),
+        KNeighborsRegressor(n_neighbors=11),
+        AdaBoostRegressor(),
+        XGBRegressor(),
+        SVR(),
+    ]
+    scores = []
     test_score = []
     train_times = []
-    names= []
+    names = []
 
     # training
     for model in models:
-        start= time.time()
-        scores.append(cross_val_score(model, X_train, y_train, 
-                                      scoring= 'r2', cv= 5).mean())
+        start = time.time()
+        scores.append(
+            cross_val_score(model, X_train, y_train, scoring="r2", cv=5).mean()
+        )
         end = time.time()
-        train_times.append(end-start)
+        train_times.append(end - start)
         names.append(model.__class__.__name__)
-    df= pd.DataFrame(scores, columns=['Score'], index= range(len(models)))
-    df.insert(1, 'Time', pd.Series(train_times))
-    df.insert(0, 'Model', pd.Series(names))
+    df = pd.DataFrame(scores, columns=["Score"], index=range(len(models)))
+    df.insert(1, "Time", pd.Series(train_times))
+    df.insert(0, "Model", pd.Series(names))
 
     best_score = 0
     best_model = models[0]
@@ -65,16 +74,18 @@ def train_and_evaluate():
         if best_score < score:
             best_score = score
             best_model = model
-        test_score.append(score) 
-    df['Test_Score'] = test_score
+        test_score.append(score)
+    df["Test_Score"] = test_score
     return best_model, df
 
+
 def saving_plotting(best_model, df):
-    pickle.dump(best_model, open('models/best_model.pkl', 'wb'))
-    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    save_path = './history/' + current_time + '.png'
+    pickle.dump(best_model, open("models/best_model.pkl", "wb"))
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    save_path = "./history/" + current_time + ".png"
     dfi.export(df, save_path)
-    dfi.export(df, './history/current_result.png')
+    dfi.export(df, "./history/current_result.png")
+
 
 if __name__ == "__main__":
     best_model, df = train_and_evaluate()
